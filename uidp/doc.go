9a02a99a0ea133e6cceb8264ff3add93ff376e55
Billy Lynch <1844673+wlynch@users.noreply.github.com>
2024-01-11 13:36:09 -0500

    Export f64ce7e6d3b76a94461057a23401451ea34dace0
    Export:  f64ce7e6d3b76a94461057a23401451ea34dace0

--- uidp/doc.go
+++
@@ -1,8 +0,0 @@
-/*
-Copyright 2022 Chainguard, Inc.
-SPDX-License-Identifier: Apache-2.0
-*/
-
-// Package uidp contains utilities for reasoning about and manipulating the
-// Chainguard IAM "UIDP" (UID Path) concept.
-package uidp
