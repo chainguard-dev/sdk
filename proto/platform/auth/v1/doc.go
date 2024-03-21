1a923d0f3815032f89c79239f8fff6dc02eab09d
Jason Hall <jason@chainguard.dev>
2024-03-21 08:44:38 -0400

    Export 6f85d0d482f340af0a987a5ccbdc39bf0c97edb8
    Export:  6f85d0d482f340af0a987a5ccbdc39bf0c97edb8

--- proto/platform/auth/v1/doc.go
+++ proto/platform/auth/v1/doc.go
@@ -6,5 +6,5 @@ SPDX-License-Identifier: Apache-2.0
 // Package v1 contains the v1 GRPC client and server definitions
 // for implementing Auth interactions for the Console.
 //
-//go:generate protoc -I . -I ../.. -I ../../.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --openapiv2_out=. auth.platform.proto
+//go:generate protoc -I . -I ../.. -I ../../.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway_opt omit_package_doc=true --openapiv2_out=. auth.platform.proto
 package v1
