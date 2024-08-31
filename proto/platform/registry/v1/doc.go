3b40a746e3f2c11553ea39be984b49f70f27ad1e
Colin Douglas <16968564+cmdpdx@users.noreply.github.com>
2024-08-30 19:03:05 -0700

    Export 9368aeaf3e9f64cdc78c389d5ae922ded112d593
    Export:  9368aeaf3e9f64cdc78c389d5ae922ded112d593

--- proto/platform/registry/v1/doc.go
+++ proto/platform/registry/v1/doc.go
@@ -7,4 +7,5 @@ SPDX-License-Identifier: Apache-2.0
 // for implementing Registry interactions.
 //
 //go:generate protoc -I . -I ../.. -I ../../.. -I ../../tenant/v1 --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway_opt omit_package_doc=true --openapiv2_out=. registry.platform.proto
+//go:generate protoc -I . -I ../.. -I ../../.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway_opt omit_package_doc=true --openapiv2_out=. vulnerabilities.platform.proto
 package v1
