727180e0e3a655a9cbd4bdbbf60d3ee7ab3a06cb
Colin Douglas <16968564+cmdpdx@users.noreply.github.com>
2025-06-03 15:25:49 -0700

    Export 13f14e3613cbdced0ffc95e66d336591d445023f
    Export:  13f14e3613cbdced0ffc95e66d336591d445023f

---
+++ proto/platform/libraries/v1/test/artifacts.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2025 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/testing/protocmp"
+
+	libraries "chainguard.dev/sdk/proto/platform/libraries/v1"
+)
+
+var _ libraries.ArtifactsClient = (*MockArtifactsClient)(nil)
+
+type MockArtifactsClient struct {
+	libraries.EntitlementsClient
+
+	OnList             []ArtifactsOnList
+	OnListVersions     []ArtifactsOnListVersions
+	OnGetArtifactCount []ArtifactsOnGetArtifactCount
+}
+
+type ArtifactsOnList struct {
+	Given *libraries.ArtifactFilter
+	List  *libraries.ArtifactList
+	Error error
+}
+
+type ArtifactsOnListVersions struct {
+	Given *libraries.ArtifactVersionFilter
+	List  *libraries.ArtifactVersionList
+	Error error
+}
+
+type ArtifactsOnGetArtifactCount struct {
+	Given  *libraries.GetArtifactCountRequest
+	Result *libraries.GetArtifactCountResponse
+	Error  error
+}
+
+func (m MockArtifactsClient) List(_ context.Context, given *libraries.ArtifactFilter, _ ...grpc.CallOption) (*libraries.ArtifactList, error) {
+	for _, o := range m.OnList {
+		if cmp.Equal(o.Given, given, protocmp.Transform()) {
+			return o.List, o.Error
+		}
+	}
+	return nil, fmt.Errorf("mock not found for %v", given)
+}
+
+func (m MockArtifactsClient) ListVersions(_ context.Context, given *libraries.ArtifactVersionFilter, _ ...grpc.CallOption) (*libraries.ArtifactVersionList, error) {
+	for _, o := range m.OnListVersions {
+		if cmp.Equal(o.Given, given, protocmp.Transform()) {
+			return o.List, o.Error
+		}
+	}
+	return nil, fmt.Errorf("mock not found for %v", given)
+}
+
+func (m MockArtifactsClient) GetArtifactCount(_ context.Context, given *libraries.GetArtifactCountRequest, _ ...grpc.CallOption) (*libraries.GetArtifactCountResponse, error) {
+	for _, o := range m.OnGetArtifactCount {
+		if cmp.Equal(o.Given, given, protocmp.Transform()) {
+			return o.Result, o.Error
+		}
+	}
+	return nil, fmt.Errorf("mock not found for %v", given)
+}
