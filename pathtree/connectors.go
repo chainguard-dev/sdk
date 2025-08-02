94a793c67feadef517c6bb7d87d3e4b47c372a82
Matt Moore <mattmoor@chainguard.dev>
2025-08-02 09:04:38 -0700

    Export 956bcd01bccbc21c0ee5d3db8498446ad2e080f7
    Export:  956bcd01bccbc21c0ee5d3db8498446ad2e080f7

---
+++ pathtree/connectors.go
@@ -0,0 +1,16 @@
+/*
+Copyright 2022 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package pathtree
+
+const tee string = "\u251c"
+const elbow string = "\u2514"
+const space string = " "
+const pipe string = "\u2502"
+
+const teeSpace string = tee + " "
+const elbowSpace string = elbow + " "
+const spaceSpace string = space + " "
+const pipeSpace string = pipe + " "
