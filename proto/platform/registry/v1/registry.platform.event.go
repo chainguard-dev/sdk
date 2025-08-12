0bc31829b5e5b9731e9d4987dc6bda6618a1942a
Markus Thömmes <markus.thoemmes@chainguard.dev>
2025-08-12 18:52:00 +0200

    Export 9e4365bed5362eacdd2c9f8939fd33a89e3ace5c
    Export:  9e4365bed5362eacdd2c9f8939fd33a89e3ace5c

--- proto/platform/registry/v1/registry.platform.event.go
+++ proto/platform/registry/v1/registry.platform.event.go
@@ -5,11 +5,13 @@ SPDX-License-Identifier: Apache-2.0
 
 package v1
 
+import "chainguard.dev/sdk/uidp"
+
 // CloudEventsExtension implements chainguard.dev/sdk/events/Extendable.CloudEventsExtension
 func (x *Repo) CloudEventsExtension(key string) (string, bool) {
 	switch key {
 	case "group":
-		return x.GetId(), true
+		return uidp.Parent(x.GetId()), true
 	default:
 		return "", false
 	}
@@ -17,5 +19,20 @@ func (x *Repo) CloudEventsExtension(key string) (string, bool) {
 
 // CloudEventsSubject implements chainguard.dev/sdk/events/Eventable.CloudEventsSubject.
 func (x *Repo) CloudEventsSubject() string {
+	return x.GetId()
+}
+
+// CloudEventsExtension implements chainguard.dev/sdk/events/Extendable.CloudEventsExtension
+func (x *DeleteRepoRequest) CloudEventsExtension(key string) (string, bool) {
+	switch key {
+	case "group":
+		return uidp.Parent(x.GetId()), true
+	default:
+		return "", false
+	}
+}
+
+// CloudEventsSubject implements chainguard.dev/sdk/events/Eventable.CloudEventsSubject.
+func (x *DeleteRepoRequest) CloudEventsSubject() string {
 	return x.GetId()
 }
