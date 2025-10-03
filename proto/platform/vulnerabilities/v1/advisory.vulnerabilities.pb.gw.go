edbd991eae6975f020e2bc71fa948b0b1ea1e897
Zack Crosley <mail@crosleyzack.com>
2025-10-03 10:53:12 -0400

    Export d22fcb21234ae48d9aa0aa9713ec967332147ceb
    Export:  d22fcb21234ae48d9aa0aa9713ec967332147ceb

--- proto/platform/vulnerabilities/v1/advisory.vulnerabilities.pb.gw.go
+++ proto/platform/vulnerabilities/v1/advisory.vulnerabilities.pb.gw.go
@@ -312,6 +312,66 @@ func local_request_Advisories_ListAdvisoryEvents_0(ctx context.Context, marshale
 
 }
 
+func request_Advisories_UpdateAdvisoryEvent_0(ctx context.Context, marshaler runtime.Marshaler, client AdvisoriesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AdvisoryEvent
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
+	}
+
+	protoReq.Id, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	msg, err := client.UpdateAdvisoryEvent(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Advisories_UpdateAdvisoryEvent_0(ctx context.Context, marshaler runtime.Marshaler, server AdvisoriesServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AdvisoryEvent
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
+	}
+
+	protoReq.Id, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	msg, err := server.UpdateAdvisoryEvent(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterAdvisoriesHandlerServer registers the http handlers for service Advisories to "mux".
 // UnaryRPC     :call AdvisoriesServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -466,6 +526,31 @@ func RegisterAdvisoriesHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		}
 
 		forward_Advisories_ListAdvisoryEvents_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("PUT", pattern_Advisories_UpdateAdvisoryEvent_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/chainguard.platform.vulnerabilities.v1.Advisories/UpdateAdvisoryEvent", runtime.WithHTTPPathPattern("/vulnerabilities/v1/advisory_events/{id=**}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Advisories_UpdateAdvisoryEvent_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Advisories_UpdateAdvisoryEvent_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -642,6 +727,28 @@ func RegisterAdvisoriesHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("PUT", pattern_Advisories_UpdateAdvisoryEvent_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/chainguard.platform.vulnerabilities.v1.Advisories/UpdateAdvisoryEvent", runtime.WithHTTPPathPattern("/vulnerabilities/v1/advisory_events/{id=**}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Advisories_UpdateAdvisoryEvent_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Advisories_UpdateAdvisoryEvent_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -657,6 +764,8 @@ var (
 	pattern_Advisories_CreateAdvisoryEvent_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 3, 0, 4, 1, 5, 3, 2, 4}, []string{"vulnerabilities", "v1", "advisories", "parent_id", "events"}, ""))
 
 	pattern_Advisories_ListAdvisoryEvents_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 3, 0, 4, 1, 5, 3, 2, 4}, []string{"vulnerabilities", "v1", "advisories", "parent_id", "events"}, ""))
+
+	pattern_Advisories_UpdateAdvisoryEvent_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 3, 0, 4, 1, 5, 3}, []string{"vulnerabilities", "v1", "advisory_events", "id"}, ""))
 )
 
 var (
@@ -671,4 +780,6 @@ var (
 	forward_Advisories_CreateAdvisoryEvent_0 = runtime.ForwardResponseMessage
 
 	forward_Advisories_ListAdvisoryEvents_0 = runtime.ForwardResponseMessage
+
+	forward_Advisories_UpdateAdvisoryEvent_0 = runtime.ForwardResponseMessage
 )
--- proto/platform/vulnerabilities/v1/test/advisory.go
+++ proto/platform/vulnerabilities/v1/test/advisory.go
@@ -22,15 +22,26 @@ type MockAdvisoriesClient struct {
 	vulnerabilities.AdvisoriesClient
 
 	OnList   []AdvisoriesOnList
+	OnGet    []AdvisoryOnGet
 	OnCreate []AdvisoriesOnCreate
 	OnUpdate []AdvisoriesOnUpdate
 	OnDelete []AdvisoriesOnDelete
+
+	OnListAdvisoryEvent   []AdvisoriesOnEventList
+	OnCreateAdvisoryEvent []AdvisoriesOnEventCreate
+	OnUpdateAdvisoryEvent []AdvisoriesOnEventUpdate
 }
 
 type AdvisoriesOnList struct {
 	Given *vulnerabilities.AdvisoryFilter
 	List  *vulnerabilities.AdvisoriesList
 	Error error
+}
+
+type AdvisoryOnGet struct {
+	Given    *vulnerabilities.AdvisoryFilter
+	Advisory *vulnerabilities.Advisory
+	Error    error
 }
 
 type AdvisoriesOnCreate struct {
@@ -50,10 +61,37 @@ type AdvisoriesOnDelete struct {
 	Error error
 }
 
+type AdvisoriesOnEventList struct {
+	Given     *vulnerabilities.AdvisoryEventFilter
+	EventList *vulnerabilities.AdvisoryEventList
+	Error     error
+}
+
+type AdvisoriesOnEventCreate struct {
+	Given        *vulnerabilities.CreateAdvisoryEventRequest
+	EventCreated *vulnerabilities.AdvisoryEvent
+	Error        error
+}
+
+type AdvisoriesOnEventUpdate struct {
+	Given        *vulnerabilities.AdvisoryEvent
+	EventUpdated *vulnerabilities.AdvisoryEvent
+	Error        error
+}
+
 func (m MockAdvisoriesClient) List(_ context.Context, given *vulnerabilities.AdvisoryFilter, _ ...grpc.CallOption) (*vulnerabilities.AdvisoriesList, error) { //nolint: revive
 	for _, o := range m.OnList {
 		if cmp.Equal(o.Given, given, protocmp.Transform()) {
 			return o.List, o.Error
+		}
+	}
+	return nil, fmt.Errorf("mock not found for %v", given)
+}
+
+func (m *MockAdvisoriesClient) Get(_ context.Context, given *vulnerabilities.AdvisoryFilter, _ ...grpc.CallOption) (*vulnerabilities.Advisory, error) {
+	for _, o := range m.OnGet {
+		if cmp.Equal(o.Given, given, protocmp.Transform()) {
+			return o.Advisory, o.Error
 		}
 	}
 	return nil, fmt.Errorf("mock not found for %v", given)
@@ -81,6 +119,33 @@ func (m MockAdvisoriesClient) Update(_ context.Context, given *vulnerabilities.A
 	for _, o := range m.OnUpdate {
 		if cmp.Equal(o.Given, given, protocmp.Transform()) {
 			return o.Updated, o.Error
+		}
+	}
+	return nil, fmt.Errorf("mock not found for %v", given)
+}
+
+func (m MockAdvisoriesClient) ListAdvisoryEvents(_ context.Context, given *vulnerabilities.AdvisoryEventFilter, _ ...grpc.CallOption) (*vulnerabilities.AdvisoryEventList, error) { //nolint: revive
+	for _, o := range m.OnListAdvisoryEvent {
+		if cmp.Equal(o.Given, given, protocmp.Transform()) {
+			return o.EventList, o.Error
+		}
+	}
+	return nil, fmt.Errorf("mock not found for %v", given)
+}
+
+func (m MockAdvisoriesClient) CreateAdvisoryEvent(_ context.Context, given *vulnerabilities.CreateAdvisoryEventRequest, _ ...grpc.CallOption) (*vulnerabilities.AdvisoryEvent, error) {
+	for _, o := range m.OnCreateAdvisoryEvent {
+		if cmp.Equal(o.Given, given, protocmp.Transform()) {
+			return o.EventCreated, o.Error
+		}
+	}
+	return nil, fmt.Errorf("mock not found for %v", given)
+}
+
+func (m MockAdvisoriesClient) UpdateAdvisoryEvent(_ context.Context, given *vulnerabilities.AdvisoryEvent, _ ...grpc.CallOption) (*vulnerabilities.AdvisoryEvent, error) {
+	for _, o := range m.OnUpdateAdvisoryEvent {
+		if cmp.Equal(o.Given, given, protocmp.Transform()) {
+			return o.EventUpdated, o.Error
 		}
 	}
 	return nil, fmt.Errorf("mock not found for %v", given)
