1a4db6583fead86bd865ec1a3be1ac94b70fddf3
Colin Douglas <16968564+cmdpdx@users.noreply.github.com>
2026-02-10 10:01:01 -0800

    Export 7c82536e7cf8963cf43fc4f841912625adf5de07
    Export:  7c82536e7cf8963cf43fc4f841912625adf5de07

--- proto/platform/auth/v1/doc.go
+++ proto/platform/auth/v1/doc.go
@@ -6,5 +6,5 @@ SPDX-License-Identifier: Apache-2.0
 // Package v1 contains the v1 GRPC client and server definitions
 // for implementing Auth interactions for the Console.
 //
-//go:generate protoc -I . -I ../.. -I ../../.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway_opt omit_package_doc=true --openapiv2_out=. auth.platform.proto
+//go:generate protoc -I . -I ../.. -I ../../.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway_opt omit_package_doc=true --openapiv2_out=. --openapiv2_opt use_allof_for_refs=true,preserve_rpc_order=true,openapi_naming_strategy=fqn,enable_rpc_deprecation=true auth.platform.proto
 package v1
