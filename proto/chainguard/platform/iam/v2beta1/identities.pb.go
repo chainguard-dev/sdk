e3be5a678b140b575f1c90d82a7c216417f452ea
Billy Lynch <1844673+wlynch@users.noreply.github.com>
2026-07-17 13:13:07 -0400

    Add a guardener service principal
    Export:  e8906aee5f7420a5ca9a98ecf44a98631816852c

--- proto/chainguard/platform/iam/v2beta1/identities.pb.go
+++ proto/chainguard/platform/iam/v2beta1/identities.pb.go
@@ -117,6 +117,8 @@ const (
 	ServicePrincipal_SERVICE_PRINCIPAL_MATERIALIZER ServicePrincipal = 9
 	// Microflow service for running a pull request's committed CI workflows in microVMs.
 	ServicePrincipal_SERVICE_PRINCIPAL_MICROFLOW ServicePrincipal = 10
+	// Guardener service, Chainguard's GitHub automation app for securing and maintaining repositories.
+	ServicePrincipal_SERVICE_PRINCIPAL_GUARDENER ServicePrincipal = 11
 )
 
 // Enum value maps for ServicePrincipal.
@@ -133,6 +135,7 @@ var (
 		8:  "SERVICE_PRINCIPAL_SKILLUP",
 		9:  "SERVICE_PRINCIPAL_MATERIALIZER",
 		10: "SERVICE_PRINCIPAL_MICROFLOW",
+		11: "SERVICE_PRINCIPAL_GUARDENER",
 	}
 	ServicePrincipal_value = map[string]int32{
 		"SERVICE_PRINCIPAL_UNSPECIFIED":        0,
@@ -146,6 +149,7 @@ var (
 		"SERVICE_PRINCIPAL_SKILLUP":            8,
 		"SERVICE_PRINCIPAL_MATERIALIZER":       9,
 		"SERVICE_PRINCIPAL_MICROFLOW":          10,
+		"SERVICE_PRINCIPAL_GUARDENER":          11,
 	}
 )
 
@@ -1709,7 +1713,7 @@ const file_chainguard_platform_iam_v2beta1_identities_proto_rawDesc = "" +
 	"\x1dRELATIONSHIP_TYPE_CLAIM_MATCH\x10\x01\x12\x1c\n" +
 	"\x18RELATIONSHIP_TYPE_STATIC\x10\x02\x12\"\n" +
 	"\x1eRELATIONSHIP_TYPE_AWS_IDENTITY\x10\x03\x12'\n" +
-	"#RELATIONSHIP_TYPE_SERVICE_PRINCIPAL\x10\x04*\x98\x03\n" +
+	"#RELATIONSHIP_TYPE_SERVICE_PRINCIPAL\x10\x04*\xb9\x03\n" +
 	"\x10ServicePrincipal\x12!\n" +
 	"\x1dSERVICE_PRINCIPAL_UNSPECIFIED\x10\x00\x12\x1e\n" +
 	"\x1aSERVICE_PRINCIPAL_COSIGNED\x10\x01\x12\x1e\n" +
@@ -1722,7 +1726,8 @@ const file_chainguard_platform_iam_v2beta1_identities_proto_rawDesc = "" +
 	"\x19SERVICE_PRINCIPAL_SKILLUP\x10\b\x12\"\n" +
 	"\x1eSERVICE_PRINCIPAL_MATERIALIZER\x10\t\x12\x1f\n" +
 	"\x1bSERVICE_PRINCIPAL_MICROFLOW\x10\n" +
-	"2\xb7\x0f\n" +
+	"\x12\x1f\n" +
+	"\x1bSERVICE_PRINCIPAL_GUARDENER\x10\v2\xb7\x0f\n" +
 	"\x11IdentitiesService\x12\xe5\x02\n" +
 	"\x0eCreateIdentity\x126.chainguard.platform.iam.v2beta1.CreateIdentityRequest\x1a).chainguard.platform.iam.v2beta1.Identity\"\xef\x01\x82\xd3\xe4\x93\x02/:\bidentity\"#/iam/v2beta1/identities/{parent=**}\x8a\xaf\xa8\xd2\x05\x06\x12\x04\n" +
 	"\x02\x85\a\x9a\xaf\xa8\xd2\x05m\n" +
--- proto/platform/iam/v1/identity.platform.pb.go
+++ proto/platform/iam/v1/identity.platform.pb.go
@@ -41,6 +41,7 @@ const (
 	ServicePrincipal_SKILLUP            ServicePrincipal = 8
 	ServicePrincipal_MATERIALIZER       ServicePrincipal = 9
 	ServicePrincipal_MICROFLOW          ServicePrincipal = 10
+	ServicePrincipal_GUARDENER          ServicePrincipal = 11
 )
 
 // Enum value maps for ServicePrincipal.
@@ -57,6 +58,7 @@ var (
 		8:  "SKILLUP",
 		9:  "MATERIALIZER",
 		10: "MICROFLOW",
+		11: "GUARDENER",
 	}
 	ServicePrincipal_value = map[string]int32{
 		"UNKNOWN":            0,
@@ -70,6 +72,7 @@ var (
 		"SKILLUP":            8,
 		"MATERIALIZER":       9,
 		"MICROFLOW":          10,
+		"GUARDENER":          11,
 	}
 )
 
@@ -1057,7 +1060,7 @@ const file_identity_platform_proto_rawDesc = "" +
 	"\x02id\x18\x02 \x01(\tR\x02id\"A\n" +
 	"\rLookupRequest\x12\x16\n" +
 	"\x06issuer\x18\x01 \x01(\tR\x06issuer\x12\x18\n" +
-	"\asubject\x18\x02 \x01(\tR\asubject*\xce\x01\n" +
+	"\asubject\x18\x02 \x01(\tR\asubject*\xdd\x01\n" +
 	"\x10ServicePrincipal\x12\v\n" +
 	"\aUNKNOWN\x10\x00\x12\f\n" +
 	"\bCOSIGNED\x10\x01\x12\f\n" +
@@ -1070,7 +1073,8 @@ const file_identity_platform_proto_rawDesc = "" +
 	"\aSKILLUP\x10\b\x12\x10\n" +
 	"\fMATERIALIZER\x10\t\x12\r\n" +
 	"\tMICROFLOW\x10\n" +
-	"2\xec\a\n" +
+	"\x12\r\n" +
+	"\tGUARDENER\x10\v2\xec\a\n" +
 	"\n" +
 	"Identities\x12\xea\x01\n" +
 	"\x06Create\x12..chainguard.platform.iam.CreateIdentityRequest\x1a!.chainguard.platform.iam.Identity\"\x8c\x01\x92A\x0f\n" +
